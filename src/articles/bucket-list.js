eeccfb730134de870a4ad83a36265dc80f9c40a6
SaiNel7 <skn29@cornell.edu>
2025-09-17 10:33:49 -0400

    blist

--- src/articles/bucket-list.js
+++ src/articles/bucket-list.js
@@ -5,11 +5,13 @@ export const article = {
   date: "February 20, 2005",
   content: `
     <ul>
-      <li>summit k2</li>
+      <li>summit annapurna i</li>
       <li>build a unicorn by thirty</li>
-      <li>rajarajeshwari amman annadhānam center</li>
+      <li>write a bestseller</li>
       <li>motorcycle through spiti valley</li>
+      <li>get a pilot's license</li>
       <li>become hokage </li>
+      // <li>rajarajeshwari amman annadhānam center</li>
     </ul>
 
   `
