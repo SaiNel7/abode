3c364fd127ed007d0317800aee1e2bdfae13958c
SaiNel7 <skn29@cornell.edu>
2025-11-10 20:37:54 -0500

    hume

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ScrollToTop from './ScrollToTop';
 import Icarus from './assets/icarus.jpg';
 
 function Home() {
-  const words = ["Aspiring Founder", "Builder", "Strategist", "Engineer", "Artist", "Writer", "Student",];
+  const words = ["Aspiring Founder", "Engineer", "Artist", "Writer", "Student",];
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
   const [currentText, setCurrentText] = useState('');
   const [isDeleting, setIsDeleting] = useState(false);
--- src/articles/index.js
+++ src/articles/index.js
@@ -1,12 +1,14 @@
 // import { article as imperialFantasies } from './imperial-fantasies';
 import { article as homecoming } from './homecoming';
 import { article as bucketList } from './bucket-list';
+import { article as hume } from './hume';
 
 
 export const blogPosts = [
   // imperialFantasies,
   homecoming,
   bucketList,
+  hume,
 ].sort((a, b) => {
   const dateA = new Date(a.date);
   const dateB = new Date(b.date);
@@ -18,4 +20,5 @@ export {
   // imperialFantasies,
   homecoming,
   bucketList,
+  hume,
 };
